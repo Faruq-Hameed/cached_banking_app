15d5d79fa70769c67a219da02a273c2cdfddb494
Faruq-Hameed <faruqhameed1@gmail.com>
2024-02-23 22:23:38 +0100

    working on bubble sort

--- assign.ts
+++ assign.ts
@@ -1,19 +1,19 @@
 function bubbleSort(arr: Array<number>, n: number): void{
     console.log({arr});
     let result = [];
+   let exchange = true
     for ( let i = 0; i < n -1; i++ ){
-        
+        //return the smaller element btw the first two elements
             let a = arr[i], b = arr[i + 1];
-            if ( a < b ){
-                result[i] = a;
-            }
-            else {
-                result[i] = b;
-            }
+
 
         for (let j = 1; j < n -1; j++ ){
-            let c = result[j];
-            if (c < )
+            let c = arr[i + 2]
+            //if the 3rd element is smaller
+            if( c < b){
+                result[i + 1] = c;
+            }
+            else if(c > b && c < a){}
         }
 
 }
