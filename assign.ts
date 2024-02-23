593f9776bfa4d50eea4d6f1df5ee97e9ff830a60
Faruq-Hameed <faruqhameed1@gmail.com>
2024-02-23 21:22:32 +0100

    started sorting algorith

--- assign.ts
+++ assign.ts
@@ -1,8 +1,16 @@
-function bubbleSort(arr: Array<number>): number[]{
+function bubbleSort(arr: Array<number>, n: number): number[]{
+    
+    console.log(arr);
     let result = [];
     for (var i = 0; i < arr.length; i++){
-        
-        
+        let pre_ele = arr[i];
+        for (var j = 1; j < arr.length; j++){
+            let next_ele = arr[j];
+            if(pre_ele < next_ele){
+                result[]  = 0
+            }
+        }
+
     }
     return arr;
 }
