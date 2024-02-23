9cb892452a5163f8aa20b6653e832d214c70d96c
Faruq-Hameed <faruqhameed1@gmail.com>
2024-02-23 21:31:29 +0100

    working on bubble sort

--- assign.ts
+++ assign.ts
@@ -1,16 +1,4 @@
 function bubbleSort(arr: Array<number>, n: number): number[]{
-    
-    console.log(arr);
-    let result = [];
-    for (var i = 0; i < n -1; i++){
-        let pre_ele = arr[i];
-        for (var j = 1; j < n; j++){
-            let next_ele = arr[j];
-            if(pre_ele < next_ele){
-                result[0]  = 0
-            }
-        }
 
-    }
-    return arr;
 }
+bubbleSort([8,1,0,6], 8)
